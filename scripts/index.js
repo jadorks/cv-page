8b48e6d6470e69da09e1091a0e06bbda2a96bc5f
jadorks <adorkorj@gmail.com>
2020-09-22 00:13:32 +0000

    Created javascript and linked to html

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("cv_form");
+
+function onSubmit(event){
+    
+    event.preventDefault();
+    alert("Hahahaha");
+}
+
+form.addEventListener("submit", onSubmit);
+
