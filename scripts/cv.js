ef8b0408ed8e6bc11967c81f7885b19bfbcb6953
jadorks <adorkorj@gmail.com>
2020-09-22 10:49:40 +0000

    Fixed insertion into html

--- scripts/cv.js
+++ scripts/cv.js
@@ -2,11 +2,11 @@ function loadData(){
 
     const name = localStorage.getItem("fname") + " " + localStorage.getItem("mid") + " " + localStorage.getItem("lname");
     
-    document.getElementById("name").innerHTML = `<p> ${name} </p>`;
-    document.getElementById("address").innerHTML = `<p> ${localStorage.getItem("address")} </p>`;
-    document.getElementById("phone").innerHTML = `<p> ${localStorage.getItem("phone")} </p>`;
-    document.getElementById("email").innerHTML = `<p> ${localStorage.getItem("email")} </p>`;
-    document.getElementById("gender").innerHTML = `<p> ${localStorage.getItem("gender")} </p>`;
+    document.getElementById("name").innerHTML = `${name}`;
+    document.getElementById("address").innerHTML = `${localStorage.getItem("address")}`;
+    document.getElementById("phone").innerHTML = `${localStorage.getItem("phone")}`;
+    document.getElementById("email").innerHTML = `${localStorage.getItem("email")}`;
+    document.getElementById("gender").innerHTML = `${localStorage.getItem("gender")}`;
 }
 
 loadData();
