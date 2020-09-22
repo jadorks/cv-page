e27deb6b6a3bc8cb3b9297d0c26fcc9e5d086319
jadorks <adorkorj@gmail.com>
2020-09-22 17:48:26 +0000

    Completed form validation

--- scripts/cv.js
+++ scripts/cv.js
@@ -2,11 +2,17 @@ function loadData(){
 
     const name = localStorage.getItem("fname") + " " + localStorage.getItem("mid") + " " + localStorage.getItem("lname");
     
-    document.getElementById("name").innerHTML = `${name}`;
-    document.getElementById("address").innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("phone").innerHTML = `${localStorage.getItem("phone")}`;
-    document.getElementById("email").innerHTML = `${localStorage.getItem("email")}`;
-    document.getElementById("gender").innerHTML = `${localStorage.getItem("gender")}`;
+    if(localStorage.getItem("filled") == 1){
+        document.getElementById("name").innerHTML = `${name}`;
+        document.getElementById("address").innerHTML = `${localStorage.getItem("address")}`;
+        document.getElementById("phone").innerHTML = `${localStorage.getItem("phone")}`;
+        document.getElementById("email").innerHTML = `${localStorage.getItem("email")}`;
+        document.getElementById("gender").innerHTML = `${localStorage.getItem("gender")}`;
+    }
+    else{
+        document.getElementById("name").innerHTML = `Please fill the form <a href = "index.html">here</a>.`;   
+    }
+    
 }
 
 loadData();
--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,11 @@ const form = document.getElementById("cv_form");
 function onSubmit(event){
     
     event.preventDefault();
-    storeFields();
-    window.location = 'cv.html';
+    if(validate()){
+        storeFields();
+        window.location = 'cv.html';
+    }
+
 }
 
 function storeFields(){
@@ -15,14 +18,62 @@ function storeFields(){
     localStorage.setItem("email", form.elements["email"].value);
     localStorage.setItem("phone", form.elements["phone"].value);
     localStorage.setItem("address", form.elements["address"].value);
+    localStorage.setItem("filled", 1);
 }
 
 function validate(){
     var phonePattern = /^[0-9]+$/;
-    var textPattern = /^[A-Za-z]+$/;
+    var textPattern = /^[A-Za-z., ]+$/;
+    var addressPattern = /^[0-9A-Za-z., ]+$/;
     var emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    const 
+    const first = form.elements["fname"].value;
+    const middle = form.elements["mid"].value;
+    const last = form.elements["lname"].value;
+    const email = form.elements["email"].value;
+    const phone = form.elements["phone"].value;
+    const address = form.elements["address"].value;
+
+    if (!first.match(textPattern) || !middle.match(textPattern) || !last.match(textPattern) || !email.match(emailPattern)
+    || !phone.match(phonePattern) || !address.match(textPattern)){
+        removeError();
+        if(!first.match(textPattern)){
+            addError("fname");
+        }
+        if(!middle.match(textPattern)){
+            addError("mid");
+        }
+        if(!last.match(textPattern)){
+            addError("lname");
+        }
+        if(!email.match(emailPattern)){
+            addError("email");
+        }
+        if(!phone.match(phonePattern)){
+            addError("phone");
+        }
+        if(!address.match(addressPattern)){
+            addError("address");
+        }
+    }
+    else{
+        removeError();
+        return true;
+    }
+}
+
+function addError(id){
+    document.getElementById(id).classList.add('error-box');
+}
+
+function removeError(){
+    document.getElementById("fname").classList.remove('error-box');
+    document.getElementById("mid").classList.remove('error-box');
+    document.getElementById("lname").classList.remove('error-box');
+    document.getElementById("email").classList.remove('error-box');
+    document.getElementById("phone").classList.remove('error-box');
+    document.getElementById("address").classList.remove('error-box');
+    
 }
 
 form.addEventListener('submit', onSubmit);
